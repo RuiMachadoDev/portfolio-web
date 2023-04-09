0784e6c3721ec327262c68138ebdcd3c01cfccc0
RM_Develop <ruifilipemachado1997@gmail.com>
2023-04-09 13:06:40 +0100

    project done

---
+++ src/components/About.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+            <div className="pb-8">
+                <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+            </div>
+
+            <p className="text-xl mt-20">
+                Graduated in Communication and Multimedia,
+                I acquired a taste for graphic design and web design.<br/>
+                I stand out for my persistence and the way I'm constantly 
+                trying to improve my skills.
+            </p>
+
+            <br/>
+
+            <p className="text-xl">
+
+            </p>
+        </div>
+
+    </div>
+  )
+};
+
+export default About;
