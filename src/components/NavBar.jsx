536dd82f5329d422a382219b7d9bdc92ebb188cf
Rui Machado <ruifilipemachado1997@gmail.com>
2024-02-07 10:18:25 +0000

    Change the navbar height.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,7 +29,7 @@ const NavBar = () => {
   ];
 
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+    <div className="flex justify-between items-center w-full h-16 px-4 text-white bg-black fixed">
       <div>
         <h1 className="text-4xl font-signature ml-2">RM Design</h1>
       </div>
